bf36abbd92734a638ca98ec69eec6f553ca87f33
huklee <lhwnotss@gmail.com>
2018-12-31 01:27:12 +0900

    add no sandbox option & showAll option

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 from weddingDayBot.weddingDayBot import *
 import time
 
+from datetime import datetime
+
 checkMonth = ["2019", "9"]
 
 checkList = [["9/7", "14:00"]
@@ -26,10 +28,16 @@ if __name__ == "__main__":
         print("usage : python main.py [slackbotToken] [showAllCheck]")
         exit()
 
+    # show option for ShowAll
     if len(sys.argv) >= 3 and sys.argv[2] == "Y":
         showAll = True
     else:
         showAll = False
+
+    # showAll when the start of the day
+    h, m = datetime.now().hour, datetime.now().minute
+    if h == 7 and m == 0:
+        showAll = True
 
     run(sys.argv[1], showAll)
     sendSlackMsgSimple(sys.argv[1], "#test", "CHECKED "+time.strftime("%Y-%m-%d %l:%M%p %Z"))                                                                                                                   
--- weddingDayBot/weddingDayBot.py
+++ weddingDayBot/weddingDayBot.py
@@ -25,7 +25,13 @@ def getWeddingDayDriver():
     caps = DesiredCapabilities().CHROME
     caps["pageLoadStrategy"] = "none"
 
-    driver = webdriver.Chrome("/home/huklee/Downloads/chromedriver", desired_capabilities=caps)
+    chrome_options = webdriver.ChromeOptions() 
+    chrome_options.add_argument('--no-sandbox') 
+    chrome_options.add_argument('--window-size=1420,1080') 
+    chrome_options.add_argument('--headless') 
+    chrome_options.add_argument('--disable-gpu')
+
+    driver = webdriver.Chrome("/home/huklee/Downloads/chromedriver", desired_capabilities=caps, chrome_options = chrome_options)
 
     return driver
 
